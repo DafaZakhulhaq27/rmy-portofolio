669106a3882130bdac8cd30d91045a5cc34cb213
dafa zakhulhaq <dafazakhulhaq@192.168.0.125>
2021-09-15 06:28:30 +0700

    initial commit

---
+++ src/content.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import button from './button.png';
+
+const Content = () => {
+    return (
+        <div className="content" >
+            <div className="desc">
+                <img
+                    style={{
+                        marginBottom :'10px'
+                    }}
+                    src={button} />
+                <p style={{
+                    color : '#B2B1B9'
+                }}># assalamu’alaikum, saya Ragil Manggalaning Yudhanto. Teman-teman saya biasaudhanto. Teman-teman saya biasa</p>
+                <p># assalamu’alaikum, saya Ragil Manggalaning Yudhanto. Teman-teman saya biasa</p>
+                <p># assalamu’alaikum, saya Ragil Manggalaning Yudhanto. Teman-teman saya biasa</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Content
---
+++ src/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <a href="#">/github</a>
+            <a href="#">/instagram</a>
+            <a href="#">/twitter</a>
+            <a href="#">/facebook</a>
+        </div>
+    )
+}
+
+export default Footer
